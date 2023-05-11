158d14080fca63e5a13a73ed5e036db96a895d93
thanujachikkala <109676222+thanujachikkala@users.noreply.github.com>
2023-05-11 19:23:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+      <!-- footer part start -->
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item"><a href="customer.php" class="nav-link px-2 text-muted">View all Customers</a></li>
+        </ul>
+        <p class="text-center text-muted">The Sparks Bank</p>
+      </footer>
+      <!-- footer part end -->
